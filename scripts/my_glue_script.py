37e06d21c358f706092069266ca119112a8a8195
bmn2 <bmn2@dxc.com>
2025-08-15 21:20:53 +0530

    updated

--- scripts/my_glue_script.py
+++ scripts/my_glue_script.py
@@ -5,17 +5,17 @@ from pyspark.context import SparkContext
 from awsglue.context import GlueContext
 from awsglue.job import Job
 
-# Get arguments
+# Get parameters
 args = getResolvedOptions(sys.argv, ['JOB_NAME', 'SOURCE_S3', 'TARGET_S3'])
 
-# Initialize Glue and Spark contexts
+# Initialize Spark/Glue
 sc = SparkContext()
 glueContext = GlueContext(sc)
 spark = glueContext.spark_session
 job = Job(glueContext)
 job.init(args['JOB_NAME'], args)
 
-# Read CSV from S3
+# Read CSV from source
 datasource = glueContext.create_dynamic_frame.from_options(
     connection_type="s3",
     connection_options={"paths": [args['SOURCE_S3']]},
@@ -23,10 +23,10 @@ datasource = glueContext.create_dynamic_frame.from_options(
     format_options={"withHeader": True}
 )
 
-# Basic transform: drop nulls
+# Transform - drop null fields
 cleaned_data = datasource.drop_null_fields()
 
-# Write result back to S3 as CSV
+# Write to target
 glueContext.write_dynamic_frame.from_options(
     frame=cleaned_data,
     connection_type="s3",
